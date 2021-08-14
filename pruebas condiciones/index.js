425328a4c8961fd8aa90d60308ebb51e60006b38
MG <marcosg1996@hotmail.com.ar>
2021-08-13 22:40:24 -0300

    pruebas condiciones

---
+++ pruebas condiciones/index.js
@@ -0,0 +1,30 @@
+
+// if( condicion ){
+//     //tarea o accion 
+// }
+
+// if(a === b) // si se cumple esta condicion
+// {
+    // console.log('se cumplio')
+    // }
+    
+    
+    // if( a === 5 ){
+        //     console.log('es igual a 10')
+        // } else{ // si no se cumple esa condicion
+        //     console.log(' no se cumplio')
+        // }
+        
+let correo = 'marcos@marcos'
+let correoForm = 'eric'
+// if = si
+// else = sino
+// else if = sino si
+
+if( correo == correoForm){
+    console.log('te dejo entrar')
+}else if(correo !== correoForm){
+    console.log('correo invalido')
+}else{
+    console.log('ocurrio un error intente de nuevo')
+}
