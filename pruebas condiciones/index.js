8500df42f14bda2390e7336551fd5c8a6ec5e19c
MG <marcosg1996@hotmail.com.ar>
2021-08-13 22:57:16 -0300

    ejercicios

--- pruebas condiciones/index.js
+++ pruebas condiciones/index.js
@@ -21,10 +21,23 @@ let correoForm = 'eric'
 // else = sino
 // else if = sino si
 
-if( correo == correoForm){
-    console.log('te dejo entrar')
-}else if(correo !== correoForm){
-    console.log('correo invalido')
-}else{
-    console.log('ocurrio un error intente de nuevo')
-}
+// if( correo == correoForm){
+//     console.log('te dejo entrar')
+// }else if(correo !== correoForm){
+//     console.log('correo invalido')
+// }else{
+//     console.log('ocurrio un error intente de nuevo')
+// }
+
+let b = 10;
+let c = 5;
+let d = 20;
+let e = '20';
+
+e = 5;
+b = '10';
+
+// si b es igual a c mostrar = 'es igual b y c'
+// si c es la mitad de b mostrar = 'c es la mitad de b'
+// si b menos d es igual a b mostrar = 'es lo mismo'
+// si e mas b es igual a 15 mostrar = 'se puede sumar' y si no es igual mostrar = 'no se puede' 
